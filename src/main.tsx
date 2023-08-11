fa70d927339dddcb367f82dd5977c306bcdb49e8
Nicolas <nicolashenrisilva@outlook.com>
2023-08-11 19:07:27 -0300

    segundo commit

--- src/main.tsx
+++ src/main.tsx
@@ -4,17 +4,22 @@ import ReactDOM from 'react-dom/client'
 import Home from './pages/Home'
 //estilização global
 import "./index.css";
-import listaServicos from './pages/ListaServicos';
+import ListaServicos from './pages/ListaServicos'
 import Footer from './components/Footer';
 import Header from './components/Header'
 
+//rotas
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-   <Header />
-   <Home /> 
-   <Footer /> 
-   {/* <listaServicos /> */}
+    <BrowserRouter>
+      <Header />
+      <Routes> {/*Indica uma lista de rotas*/}
+        <Route path='/' element={<Home />} />
+        <Route path= 'lista/servicos' element= {<ListaServicos />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
 )
